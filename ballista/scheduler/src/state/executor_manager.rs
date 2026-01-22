939286247347d8e009cfa5d536ea993324676994
Luke Kim <80174+lukekim@users.noreply.github.com>
2026-01-21 21:19:01 -0800

    Fix Clippy and lint

--- ballista/scheduler/src/state/executor_manager.rs
+++ ballista/scheduler/src/state/executor_manager.rs
@@ -20,7 +20,6 @@ use std::time::Duration;
 use ballista_core::error::BallistaError;
 use ballista_core::error::Result;
 use ballista_core::serde::protobuf;
-use ballista_core::utils::GrpcClientConfig;
 use log::trace;
 
 use crate::cluster::{BoundTask, ClusterState, ExecutorSlot};
@@ -37,8 +36,7 @@ use ballista_core::serde::protobuf::{
 use ballista_core::serde::scheduler::{ExecutorData, ExecutorMetadata};
 
 use ballista_core::utils::{
-    GrpcClientConfig, create_grpc_client_connection, create_grpc_client_endpoint,
-    get_time_before,
+    GrpcClientConfig, create_grpc_client_endpoint, get_time_before,
 };
 
 use dashmap::DashMap;
@@ -465,7 +463,7 @@ impl ExecutorManager {
     async fn get_client(
         &self,
         executor_id: &str,
-        grpc_client_config: &GrpcClientConfig,
+        _grpc_client_config: &GrpcClientConfig,
     ) -> Result<ExecutorGrpcClient<Channel>> {
         let client = self.clients.get(executor_id).map(|value| value.clone());
 
